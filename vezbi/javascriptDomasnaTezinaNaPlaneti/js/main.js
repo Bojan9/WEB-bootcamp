c3a2d27b2fa460e48f8805aa8a4a28ac952ef129
Bojan9 <9spasovski@gmail.com>
2017-03-24 00:10:35 +0100

    del od domasnata

---
+++ vezbi/javascriptDomasnaTezinaNaPlaneti/js/main.js
@@ -0,0 +1,18 @@
+
+var mesecina = 0.2;
+var mars = 0.4;
+var jupiter = 2.4;
+
+var tezina;
+
+function presmetaj(){
+    if(tezina =< 1){
+
+    return(tezina * mesecina);
+    return(tezina * mars);
+    return(tezina * jupiter);
+    }
+    else{
+        console.log("Ti ne postois");
+    }
+}
