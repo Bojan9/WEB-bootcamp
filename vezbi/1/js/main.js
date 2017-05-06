612079fe7719e66ebc3558a057d2079703d4acb3
Bojan9 <9spasovski@gmail.com>
2017-05-06 12:48:49 +0200

    digitron

--- vezbi/1/js/main.js
+++ vezbi/1/js/main.js
@@ -2,8 +2,11 @@ var vlez = document.getElementById("vlez");
 var kopce= document.getElementById("presmetaj");
 var izlez= document.getElementById("izlez");
 var znak = false;
+var presmetki = document.getElementById("presmetki");
+var ednakvo = false;
 
 function presmetaj (){
+     ednakvo = true;
 
 
 
@@ -16,9 +19,18 @@ function presmetaj (){
     console.log(rezultat);
 
     izlez.value = rezultat;
+
+    var pres = vrednost + "=" + rezultat + "<br/>";
+
+    presmetki.innerHTML = presmetki.innerHTML + pres;
 }
 
 function vnesi(broj){
+
+    if( ednakvo){
+        vlez.value = '';
+        ednakvo = false;
+    }
 
     if( broj == '+' || broj == '-'){
         if(znak){
@@ -31,11 +43,12 @@ function vnesi(broj){
     else{
         znak=false;
         vlez.value = vlez.value + broj;
-
     }
 
 
 
     console.log(broj);
 
+
 }
+
