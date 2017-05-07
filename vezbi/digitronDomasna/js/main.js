0ce2aafcb364abc6659cbaea3ac4e4422d5dfa16
Bojan9 <9spasovski@gmail.com>
2017-05-07 17:21:54 +0200

    digitron domasna

--- vezbi/digitronDomasna/js/main.js
+++ vezbi/digitronDomasna/js/main.js
@@ -1,54 +1,43 @@
 var vlez = document.getElementById("vlez");
 var kopce= document.getElementById("presmetaj");
 var izlez= document.getElementById("izlez");
-var znak = false;
 var presmetki = document.getElementById("presmetki");
+var znak = false;
 var ednakvo = false;
 
 function presmetaj (){
-     ednakvo = true;
-
-
+    ednakvo = true;
 
     var vrednost = vlez.value;
-
-    console.log(vrednost);
-
     var rezultat = eval(vrednost);
-
-    console.log(rezultat);
-
     izlez.value = rezultat;
 
     var pres = vrednost + "=" + rezultat + "<br/>";
-
     presmetki.innerHTML = presmetki.innerHTML + pres;
 }
 
 function vnesi(broj){
 
-    if( ednakvo){
+    if(ednakvo){
+
         vlez.value = '';
         ednakvo = false;
     }
 
-    if( broj == '+' || broj == '-'){
+    if( broj == '+' || broj == '-' || broj == '*' || broj == '/' || broj == '%' ){
+
         if(znak){
         }
         else{
+
             znak = true;
             vlez.value = vlez.value + broj;
         }
     }
     else{
+
         znak=false;
         vlez.value = vlez.value + broj;
     }
-
-
-
-    console.log(broj);
-
-
 }
 
