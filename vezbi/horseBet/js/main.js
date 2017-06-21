37e2272cd23ae45203552217848ffe3811fd01bd
Bojan9 <9spasovski@gmail.com>
2017-06-21 18:22:50 +0200

    Horse bet

---
+++ vezbi/horseBet/js/main.js
@@ -0,0 +1,55 @@
+var konji = [
+    {
+        "ime" : 'crni',
+        "boja" : '#2d221a',
+        "broj" : 1,
+        "brzina" : 40,
+        "masa" : 130,
+        "dolzNoze": 45
+    },
+    {
+        "ime" : 'belco',
+        "boja" : '#fd3',
+        "broj" : 2,
+        "brzina" : 36,
+        "masa" : 120,
+        "dolzNoze" : 39
+    },
+    {
+        "ime" : 'bojan',
+        "boja" : '#343s',
+        "broj" : 3,
+        "brzina" : 59,
+        "masa" : 115,
+        "dolzNoze" : 43
+    },
+    {
+        "ime" : 'aleksandar',
+        "boja" : '#23s2',
+        "broj" : 4,
+        "brzina" : 29,
+        "masa" : 149,
+        "dolzNoze" : 55,
+    }
+]
+
+var staza = [
+    {
+        "lenti" : 4,
+        "dolzina" : 1000,
+        "podloga" : 'zemja'
+    },
+    {
+        "lenti" : 4,
+        "dolzina" : 1500,
+        "podloga" : 'pesok'
+    }
+]
+
+var kladilnica = {
+
+    "koeficient" : 2,
+    "minVlog" : 100,
+    "maxVlog" : 1000000
+}
+
