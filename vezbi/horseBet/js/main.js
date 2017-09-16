824fc7037566f135edb34b0de473995c947a2bd8
Bojan9 <9spasovski@gmail.com>
2017-09-16 15:56:00 +0200

    animacija na konjite

--- vezbi/horseBet/js/main.js
+++ vezbi/horseBet/js/main.js
@@ -1,75 +1,99 @@
 var konji = [
     {
-        "ime" : 'crni',
-        "boja" : '#2d221a',
-        "broj" : 1,
-        "brzina" : 40,
-        "masa" : 130,
+        "ime": 'crni',
+        "boja": '#2d221a',
+        "broj": 1,
+        "brzina": 40,
+        "masa": 130,
         "dolzNoze": 45
     },
     {
-        "ime" : 'belco',
-        "boja" : '#fd3',
-        "broj" : 2,
-        "brzina" : 36,
-        "masa" : 120,
-        "dolzNoze" : 39
+        "ime": 'belco',
+        "boja": '#fd3',
+        "broj": 2,
+        "brzina": 36,
+        "masa": 120,
+        "dolzNoze": 39
     },
     {
-        "ime" : 'bojan',
-        "boja" : '#343s',
-        "broj" : 3,
-        "brzina" : 59,
-        "masa" : 115,
-        "dolzNoze" : 43
+        "ime": 'bojan',
+        "boja": '#343s',
+        "broj": 3,
+        "brzina": 59,
+        "masa": 115,
+        "dolzNoze": 43
     },
     {
-        "ime" : 'aleksandar',
-        "boja" : '#23s2',
-        "broj" : 4,
-        "brzina" : 29,
-        "masa" : 149,
-        "dolzNoze" : 55,
+        "ime": 'aleksandar',
+        "boja": '#23s2',
+        "broj": 4,
+        "brzina": 29,
+        "masa": 149,
+        "dolzNoze": 55,
     }
 ]
 
 var staza = [
     {
-        "lenti" : 4,
-        "dolzina" : 1000,
-        "podloga" : 'zemja'
+        "lenti": 4,
+        "dolzina": 1000,
+        "podloga": 'zemja'
     },
     {
-        "lenti" : 4,
-        "dolzina" : 1500,
-        "podloga" : 'pesok'
+        "lenti": 4,
+        "dolzina": 1500,
+        "podloga": 'pesok'
     }
 ]
 
 var kladilnica = {
 
-    "koeficient" : 2,
-    "minVlog" : 100,
-    "maxVlog" : 1000000
+    "koeficient": 2,
+    "minVlog": 100,
+    "maxVlog": 1000000
 }
 
 var prethodnaSlika = 0;
+var interval = setInterval(function () {
+    switch (prethodnaSlika) {
+    case 0:
+        iterator(1);
+        break;
+    case 1:
+        iterator(2);
+        break;
+    case 2:
+        iterator(3);
+        break;
+    case 3:
+        iterator(0);
+        prethodnaSlika = -1;
+        break;
+    }
+    prethodnaSlika++;
+}, 100)
 
-var interval = setInterval(function(){
-    if(prethodnaSlika == 0){
-        $('.konj').css('background-position-x', '100px');
+function iterator(redenBroj) {
+    var i = 0;
+    for (i = 0; i < 4; i++) {
+        var konj = $($('.konj img')[i]);
+        konj.attr('src', '../sliki/k' + i + '_' + redenBroj + '.png');
+        konj.css('margin-left', parseInt(konj.css('margin-left')) + Math.floor((Math.random() * 10)));
+        if (parseInt(konj.css('margin-left')) >= 425) {
+            clearInterval(interval);
+        }
     }
-    if(prethodnaSlika == 100){
-        $('.konj').css('background-position-x', '200px');
-    }
-    if(prethodnaSlika == 200){
-        $('.konj').css('background-position-x', '300px');
-    }
-    if(prethodnaSlika == 300){
-        $('.konj').css('background-position-x', '0px');
-    }
-    prethodnaSlika += 100;
-    if(prethodnaSlika == 400){
-        prethodnaSlika = 0;
-    }console.log(prethodnaSlika);
-},100)
+}
+
+//function iterator(redenBroj) {
+//    var i = 0;
+//    while(i<4){
+//        var konj =$($('.konj img')[i]);
+//        konj.attr('src', '../sliki/k' + i + '_' + redenBroj + '.png');
+//        konj.css('margin-left', parseInt(konj.css('margin-left')) + Math.floor((Math.random() * 10)));
+//        i++;
+//        if(parseInt(konj.css('margin-left'))>=425){
+//            clearInterval(interval);
+//        }
+//    }
+//}
